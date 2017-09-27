a278394a744e64913d1a0ef35d8da7b4583dbcc6
Pretty <pskpretty@gmail.com>
2017-09-27 12:03:05 +1000

    set up done

--- src/main/java/com/qut/routeOptimizerApplication/HomeController.java
+++ src/main/java/com/qut/routeOptimizerApplication/HomeController.java
@@ -1,22 +1,9 @@
-/**
- *  Copyright 2005-2016 Red Hat, Inc.
- *
- *  Red Hat licenses this file to you under the Apache License, version
- *  2.0 (the "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied.  See the License for the specific language governing
- *  permissions and limitations under the License.
- */
+
 package com.qut.routeOptimizerApplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,13 +19,14 @@ import com.graphhopper.util.Instruction;
 import com.graphhopper.util.shapes.GHPoint;
 import com.qut.routeOptimizerApplication.Bean.Location;
 import com.qut.routeOptimizerApplication.Bean.UploadInvoiceBean;
+
 @RestController
 public class HomeController {
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
-	    return "index";
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		System.out.println("inside welcomr");
+		return "welcome";
 	}
-	
 	@RequestMapping(value = "/save",method = RequestMethod.POST)
 	public ModelAndView getDistance(@ModelAttribute("locations") UploadInvoiceBean uploadInvoiceBean) {
 		String s = "";
@@ -63,7 +51,7 @@ public class HomeController {
 		request.putHint("instructions", true);
 		request.setVehicle("car"); 
 		GHResponse ghResponse = graphHopper.route(request);
-		System.out.println("hello"+request.toString());
+		System.out.println("hello"+ghResponse.toString());
 		if (ghResponse.getInstructions() != null) {
 			for (Instruction i : ghResponse.getInstructions()) {
 				s += "------>\ntime <long>: " + i.getTime() + "\n" + "name: street name" + i.getName() + "\n"
--- src/main/java/com/qut/routeOptimizerApplication/RouteOptimzerProperties.java
+++ src/main/java/com/qut/routeOptimizerApplication/RouteOptimzerProperties.java
@@ -1,8 +1,8 @@
 package com.qut.routeOptimizerApplication;
 
 public final class RouteOptimzerProperties {
-	public  final String osmFilePath = "/Users/prettykurian/Desktop/routeOptimizerApplication/src/main/resources/osmMap/australia.pbf";
-	 public  final String hopperDirectory = "/Users/prettykurian/Desktop/routeOptimizerApplication/src/main/resources/gh car";
-            
+	public  final String osmFilePath = "C:\\Users\\pretty\\Desktop\\routeOptimizerApplication\\src\\main\\resources\\osmMap\\australia.pbf";
+	 public  final String hopperDirectory = "C:\\Users\\pretty\\Desktop\\routeOptimizerApplication\\src\\main\\resources\\gh car";
+             
 
 }
