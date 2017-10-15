97e6ba87c0a4339604a55d901efab3723626b773
Pretty <pskpretty@gmail.com>
2017-10-15 16:00:03 +1000

    js fix done

--- src/main/webapp/modules/results/resultsController.js
+++ src/main/webapp/modules/results/resultsController.js
@@ -1,60 +1,108 @@
-angular.module('routeOptimizer')
-    .controller('resultsController', function ($timeout, $http) {
+angular
+		.module('routeOptimizer')
+		.controller(
+				'resultsController',
+				function($timeout, $http) {
+var map;
+					var vm = this;
+					vm.loading = false;
+					vm.selectedIndex = -1;
 
-        var vm = this;
-        vm.loading = false;
-        vm.selectedIndex = -1;
+					vm.selectFile = selectFile;
 
-        vm.selectFile = selectFile;
+					vm.fileNameObj = {
 
-        vm.fileNameObj = {
-                
-                    "fileName": null
-                 };
-        init();
+						"fileName" : null
+					};
+					init();
 
-        function init() {
-            fetchList();
-           
-            $timeout(function () {
-            	 var map;
-                 var vehicleRouteLayerGroup;
-                 var intervalTimer;
-               	  var mymap = L.map('mapid',{
-               		    center: [-25.274398, 133.7751],
-               		    zoom: 15
-               		    });
-               	  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-               		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-               		    }).addTo(mymap);
-            });
-        }
+					function init() {
+						fetchList();
 
-        function fetchList() {
-        	var  url = "http://localhost:8080/getSolvedFileList";
-            $http.get(url).success(function (response) {
-            	vm.fileList=response;
-            }).error(function (err) {
+						$timeout(function() {
+							var vehicleRouteLayerGroup;
+							var intervalTimer;
+							map = L.map('mapid', {
+								center : [ -25.274398, 133.7751 ],
+								zoom : 15
+							});
+							L
+									.tileLayer(
+											'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+											{
+												attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+											}).addTo(map);
+						});
+					}
 
-            });
+					function fetchList() {
+						var url = "http://localhost:8080/getSolvedFileList";
+						$http.get(url).success(function(response) {
+							vm.fileList = response;
+						}).error(function(err) {
 
-            //vm.fileList = ['File name 1', 'File name 2'];
-        }
+						});
 
-        function selectFile(index) {
-            vm.selectedIndex = index;
-            vm.loading = true;
-            var  url = "http://localhost:8080/getSolved";
-            vm.fileNameObj.fileName=vm.fileList[index];
-           //alert(obj.fileName);
-            $http.post(url, vm.fileNameObj).success(function (response) {
-                vm.loading = false;
-                vm.mapData = response;
-            }).error(function (err) {
-              
-                vm.loading = false;
-                alert('some error occured');
-            });
-        }
+						//vm.fileList = ['File name 1', 'File name 2'];
+					}
+					ajaxError = function(jqXHR, textStatus, errorThrown) {
+						console.log("Error: " + errorThrown);
+						console.log("TextStatus: " + textStatus);
+						console.log("jqXHR: " + jqXHR);
+						alert("Error: " + errorThrown);
+					};
+		
+					function selectFile(index) {
+						vm.selectedIndex = index;
+						vm.loading = true;
+						var url = "http://localhost:8080/getSolved";
+						vm.fileNameObj.fileName = vm.fileList[index];
+						$http.post(url, vm.fileNameObj).success(		
+							function(solution) {
+								map = L.map('mapid', {
+									center : [ -25.274398, 133.7751 ],
+									zoom : 15
+								});
+								L
+										.tileLayer(
+												'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+												{
+													attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+												}).addTo(map);
+							
+								 var markers = [];
+								console.log(solution.customerList);
+									angular.forEach(solution.customerList,
+											function(index, customer) {
+												var customerIcon = L
+														.divIcon({
+															iconSize : new L.Point(
+																	20, 20),
+															className : "vehicleRoutingCustomerMarker",
+															html : "<span>"
+																	+ index.demand
+																	+ "</span>"
+														});
+												var marker = L.marker([
+													index.latitude,
+													index.longitude ], {
+													icon : customerIcon
+												});
+												marker.addTo(map).bindPopup(
+														index.locationName
+																+ "</br>Deliver "
+																+ index.demand
+																+ " items.");
+												markers.push(marker);
+											});
+							map.fitBounds(L.featureGroup(markers).getBounds());
+									vm.loading = false;
+									vm.mapData = solution;
+								}).error(function(jqXHR, textStatus, errorThrown) {
+									ajaxError(jqXHR, textStatus, errorThrown);
+							vm.loading = false;
+							alert('some error occured');
+						});
+					}
 
-    });
+				});
